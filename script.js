90d32869abdfe7ed7df0ce35066bc7fc9122b673
Raymond Zhong <rzhong669@gmail.com>
2022-07-12 07:48:44 -0500

    Assign password input to value

---
+++ script.js
@@ -0,0 +1,7 @@
+const pass = document.querySelector('#password')
+const cpass = document.querySelector('#cpassword')
+
+// cpass.addEventListener('focusout', function() {
+//   if(cpass.value !== pass.value)
+  
+// } 
