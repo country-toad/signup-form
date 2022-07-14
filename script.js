0b194f3ae8b94304404deed16009d6e95fc6e924
Raymond Zhong <rzhong669@gmail.com>
2022-07-14 16:51:59 -0500

    Add focusout listener to password input as well as confirm password

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ cpass.addEventListener('focusout', function() {
     pass.classList.remove('invalid');
   }
 })
+
+pass.addEventListener('focusout', function() {
+  if(cpass.value !== pass.value) {
+    pass.classList.add('invalid');
+    cpass.classList.add('invalid');
+  }
+  else {
+    cpass.classList.remove('invalid');
+    pass.classList.remove('invalid');
+  }
+})
