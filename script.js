a8b28c86d51789750b5c795a37da012b9f698c69
Raymond Zhong <rzhong669@gmail.com>
2022-07-14 13:13:07 -0500

    Add password invalid check on focusout

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const pass = document.querySelector('#password')
 const cpass = document.querySelector('#cpassword')
 
-// cpass.addEventListener('focusout', function() {
-//   if(cpass.value !== pass.value)
-  
-// } 
+cpass.addEventListener('focusout', function() {
+  if(cpass.value !== pass.value) {
+    pass.classList.add('invalid');
+    cpass.classList.add('invalid');
+  }
+  else {
+    cpass.classList.remove('invalid');
+    pass.classList.remove('invalid');
+  }
+})
